12cce70c50c494fe206097459a8738649bacd394
Tamas <tamas.fehertoi@gmail.com>
2025-02-19 16:17:08 -0500

    languageswitcher css

--- src/app/components/LanguageSwitcher.jsx
+++ src/app/components/LanguageSwitcher.jsx
@@ -1,40 +1,49 @@
 "use client";
+
+import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { FaGlobe } from "react-icons/fa"; // Földgömb ikon a React Icons csomagból
 import "../[locale]/styles/language-switcher.css";
 
 const LanguageSwitcher = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const pathname = usePathname();
   const languages = ["en", "hu", "esp"];
 
   const changeLanguage = (locale) => {
     const segments = pathname.split("/").filter(Boolean);
-
-    // Ha az első szegmens a nyelv, módosítsuk
     if (languages.includes(segments[0])) {
       segments[0] = locale;
     } else {
-      segments.unshift(locale); // Ha nincs nyelv, hozzáadjuk
+      segments.unshift(locale);
     }
-
-    // Az új URL generálása
     const newPathname = `/${segments.join("/")}`;
-
-    // A `router.push` metódust használva navigálj az új URL-re
     router.push(newPathname);
+    setIsOpen(false);
   };
 
   return (
     <div className="lang-switcher-container">
-      {languages.map((lang) => (
-        <button
-          className="lang-switcher-btns"
-          key={lang}
-          onClick={() => changeLanguage(lang)}
-        >
-          {lang.toUpperCase()}
-        </button>
-      ))}
+      {/* Földgömb ikon gomb */}
+      <button className="globe-btn" onClick={() => setIsOpen(!isOpen)}>
+        <FaGlobe size={24} />
+      </button>
+
+      {/* Lenyíló menü */}
+      {isOpen && (
+        <div className="dropdown-menu">
+          {languages.map((lang) => (
+            <button
+              key={lang}
+              className="lang-switcher-btn"
+              onClick={() => changeLanguage(lang)}
+            >
+              {lang.toUpperCase()}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
