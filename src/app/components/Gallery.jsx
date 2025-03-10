e0545df3fa73f5d8fa4931425a193a432776ca36
Tamas <tamas.fehertoi@gmail.com>
2025-03-10 15:56:01 -0500

    gallery css

--- src/app/components/Gallery.jsx
+++ src/app/components/Gallery.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import "./Gallery.css";
-import Image1 from "../../assets/Images/image1.webp";
-import Image2 from "../../assets/Images/Gallery/image2.webp";
-import Image3 from "../../assets/Images/Gallery/image3.webp";
-import Image4 from "../../assets/Images/Gallery/image4.webp";
-import Image5 from "../../assets/Images/Gallery/image5.webp";
-import Image6 from "../../assets/Images/Gallery/image6.webp";
+import "../[locale]/styles/Gallery.css";
 
 const Gallery = () => {
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
 
   const images = [
-    { id: 1, src: Image1, alt: "Gallery Image 1" },
-    { id: 2, src: Image2, alt: "Gallery Image 2" },
-    { id: 3, src: Image3, alt: "Gallery Image 3" },
-    { id: 4, src: Image4, alt: "Gallery Image 4" },
-    { id: 5, src: Image5, alt: "Gallery Image 5" },
-    { id: 6, src: Image6, alt: "Gallery Image 6" },
+    { id: 1, src: "/images/gallery1.webp", alt: "Gallery Image 1" },
+    { id: 2, src: "/images/gallery2.webp", alt: "Gallery Image 2" },
+    { id: 3, src: "/images/gallery3.webp", alt: "Gallery Image 3" },
+    { id: 4, src: "/images/gallery4.webp", alt: "Gallery Image 4" },
+    { id: 5, src: "/images/gallery5.webp", alt: "Gallery Image 5" },
+    { id: 6, src: "/images/gallery6.webp", alt: "Gallery Image 6" },
   ];
-
   const closeModal = () => setSelectedImageIndex(null);
 
   const showPreviousImage = () => {
