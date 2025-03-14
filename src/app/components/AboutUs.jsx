79897743502b5f901eab7eccee9dd429bde5daf4
Tamas <tamas.fehertoi@gmail.com>
2025-03-13 19:04:06 -0500

    about us

--- src/app/components/AboutUs.jsx
+++ src/app/components/AboutUs.jsx
@@ -1,9 +1,24 @@
 import "../[locale]/styles/about-us.css";
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
     <section className="about-us">
-      <img src="" alt="" />
+      <div className="about-layer"></div>
+      <Image
+        className="about-image1"
+        src="/Images/about-image1.webp"
+        width={1080}
+        height={1920}
+        alt=""
+      ></Image>
+      <Image
+        className="about-image2"
+        src="/Images/about-image2.png"
+        width={1080}
+        height={1080}
+        alt=""
+      ></Image>
     </section>
   );
 };
